18db11072c83af10370ef2ba4b9594811a480a14
CristianHernandez <cri.hernandezf@duocuc.cl>
2026-06-17 18:13:42 -0400

    Implementacion swagger en 6 microservicios y operativo falta agregar notaciones de detalles

--- service-calendario/service-calendario/src/main/java/com/nubemedica/service_calendario/controller/CalendarioController.java
+++ service-calendario/service-calendario/src/main/java/com/nubemedica/service_calendario/controller/CalendarioController.java
@@ -14,6 +14,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/calendario")
+@CrossOrigin(origins = "http://localhost:8081")
 public class CalendarioController {
 
     @Autowired
